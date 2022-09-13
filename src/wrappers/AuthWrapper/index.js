5806efd7ea5043a31fed824aa4d9125fa1ec8629
Dev02 - Ninasoft <dev02.ninasoft@gmail.com>
2022-09-13 18:03:04 +0700

    Add route login page

--- src/wrappers/AuthWrapper/index.js
+++ src/wrappers/AuthWrapper/index.js
@@ -1,37 +1,23 @@
-import React, {Fragment, Suspense} from 'react';
-import {Routes, Route} from 'react-router-dom';
-import {PrivateRoutes} from '../../routes';
-import Header from '../../components/Header';
-import Nav from '../../components/Nav';
+import React, { Fragment, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { PrivateRoutes } from "../../routes";
+import Header from "../../components/Header";
+import Nav from "../../components/Nav";
 
 export default function App() {
   return (
-    <Fragment>
-      <Header/>
-      <Nav/>
-      <div className="main-wrapper">
-        <Suspense fallback={null}>
-          <Routes>
-            {PrivateRoutes?.map((item) => (
-              <Route
-                key={item.key}
-                path={item?.path}
-                element={item.page}
-              >
-                {
-                  !!item.children && item.children.map((child) => (
-                    <Route
-                      key={child.key}
-                      path={child.path}
-                      element={child.page}
-                    />
-                  ))
-                }
-              </Route>
-            ))}
-          </Routes>
-        </Suspense>
-      </div>
-    </Fragment>
-  )
+    
+    <Suspense fallback={null}>
+      <Routes>
+        {PrivateRoutes?.map((item) => (
+          <Route key={item.key} path={item?.path} element={item.page}>
+            {!!item.children &&
+              item.children.map((child) => (
+                <Route key={child.key} path={child.path} element={child.page} />
+              ))}
+          </Route>
+        ))}
+      </Routes>
+    </Suspense>
+  );
 }

f9d2a5ee5fa99d3f1e719623998222f660c34389
Thomas Nguyen <towhidul.ninasoft@gmail.com>
2022-09-19 17:16:41 +0700

    delete MinhHoaGiaTri folder

--- src/pages/FinancialSolution/MinhHoaGiaTriUyThac/CircleData.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const CircleData = ({ data }) => {
-  return (
-    <div className="circle_data_container">
-      <div className="circle_data">{data}</div>
-    </div>
-  );
-};

8da677d641f435602419bf21a9f8162f2465fce4
Seo Lau A <seola.ninasoft@gmail.com>
2022-09-28 19:06:53 +0700

    integrated APIs consult

--- src/slices/consult.js
+++ src/slices/consult.js
@@ -6,7 +6,7 @@ const initialState = { isReload: false, data: [],total:0 };
 export const getConsult = createAsyncThunk('consult/getall', async (params , { rejectWithValue }) => {
   try {
     const res = await getAll(params);
-    console.log(params);
+    // console.log(params);
     return { data: res.data, message: '' };
   } catch (error) {
     return rejectWithValue(error.response.data);

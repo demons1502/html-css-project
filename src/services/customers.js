2469a15e00cf9b7d092780602f4326f5b6e9b724
cuonglvm <cuonglvmphp@gmail.com>
2022-10-07 09:40:27 +0700

    add sendpatch

--- src/services/customers.js
+++ src/services/customers.js
@@ -1,4 +1,4 @@
-import { sendGet } from './axios';
+import { sendGet, sendPatch } from './axios';
 import { ENDPOINT } from '../config/endpoint';
 
 export const getCustomers = (payload) => sendGet(ENDPOINT.customers, payload);
